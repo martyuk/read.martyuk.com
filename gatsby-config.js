05e62667f4ccb844dc15355d67edc1bc1f4604be
martyuk <petr@martyuk.com>
2020-03-19 00:40:24 +0300

    minor stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -101,7 +101,6 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
